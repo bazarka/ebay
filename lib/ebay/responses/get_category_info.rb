42e5c140c469e2cce00859a3a352f69a6355e7b4
Przemysław Zając <zajac@rtechnologies.pl>
2014-06-18 13:59:23 +0200

    changed time_node to text_node

--- lib/ebay/responses/get_category_info.rb
+++ lib/ebay/responses/get_category_info.rb
@@ -19,12 +19,11 @@ module Ebay # :nodoc:
       array_node :categories, 'CategoryArray', 'Category', :class => Category, :default_value => []
       numeric_node :category_count, 'CategoryCount', :optional => true
       time_node :update_time, 'UpdateTime', :optional => true
-      time_node :update_time, 'UpdateTime', :optional => true
       text_node :ack, 'Ack', :optional => true
       text_node :correlation_id, 'CorrelationID', :optional => true
       array_node :errors, 'Errors', :class => Error, :default_value => []
       time_node :timestamp, 'Timestamp', :optional => true
-      time_node :version, 'Version', :optional => true
+      text_node :version, 'Version', :optional => true
     end
   end
 end
